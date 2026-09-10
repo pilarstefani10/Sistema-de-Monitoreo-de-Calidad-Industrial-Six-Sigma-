76c2afb6b0b5d294c5a3c0d05d98dbf1bb34b34b
Pilar <phernando@itba.eduar>
2026-09-10 19:44:01 -0300

    Eliminar la variable qmuestras y la validación de límite de muestras en la clase Lote; renombrar ProcedimientoVisual a ProcedimientoDimensional y agregar lógica de evaluación con tolerancia.

--- Lote.py
+++ Lote.py
@@ -2,7 +2,6 @@
 from Muestra import Muestra
 class Lote:
     nextid = 1
-    qmuestras = 0
     def __init__(self, cantidad_fabricada):
 
         if not isinstance (cantidad_fabricada, int) or cantidad_fabricada <= 0:
@@ -15,8 +14,6 @@ class Lote:
         self.muestras = []
 
     def crear_muestra(self):
-        if self.qmuestras == 20:
-            raise ValueError("No se pueden crear más de 20 muestras por lote")
         cantidad_muestra = round(self.cantidad_fabricada * 0.05)
         muestra = Muestra(
             str(self.ID) +"M" + str(len(self.muestras) + 1),
--- ProcedimientoDimensional.py
+++ ProcedimientoDimensional.py
@@ -1,7 +1,21 @@
 from Procedimiento import Procedimiento
-class ProcedimientoVisual(Procedimiento):
-    def __init__(self, nombre, categoria_equipo_requerida, certificacion_requerida, limite_gravedad, criterios, valor_esperado):
-        super().__init__(nombre, categoria_equipo_requerida, certificacion_requerida, limite_gravedad)
-        if not isinstance(criterios, list) or len(criterios) == 0:
-            raise ValueError("Los criterios deben ser una lista no vacía.")
+class ProcedimientoDimensional(Procedimiento):
+    def __init__(self, nombre, categoria_equipo_requerida, certificacion_requerida, limite_gravedad, valor_esperado, tolerancia):
+            super().__init__(nombre, categoria_equipo_requerida, certificacion_requerida, limite_gravedad)
+            self.valor_esperado = valor_esperado
+            self.tolerancia = tolerancia
+    
+    def evaluar(self, observaciones):
+        defectos = []
+    
+        minimo = self.valor_esperado - self.tolerancia
+        maximo = self.valor_esperado + self.tolerancia
+
+        cumple= lambda valor: minimo <= valor <= maximo
+        for valor in observaciones:
+            if valor < minimo or valor > maximo:
+                    # crear Defecto
+                pass
+    
+            return defectos
         #Criterios sería un diccionario con la estructura: {"criterio1": valor1, "criterio2": valor2, ...}
